39dea5fb1a0725c0823aa767c4fdb1ed1d7bfa67
DESKTOP-67622L5\adems <adem.buran@ug.bilkent.edu.tr>
2020-10-24 10:41:58 +0300

    master: initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("ömerin babası");
+        System.out.println( "sıçarım böyle işe");
+        int x = 8;
+        System.out.println( x + " sıçma böyle işe");
+        ObjeOynama a = new ObjeOynama("tako");
+        System.out.println( a.getName());
+        a.setName("muzo");
+        System.out.println( a.getName());
+        System.out.println("hello git");
+
+	// write your code here
+    }
+}
---
+++ src/com/company/ObjeOynama.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class ObjeOynama {
+    String name;
+    public ObjeOynama( String ado)
+    {
+        name = ado;
+    }
+    public String getName()
+    {
+        return name;
+    }
+    public void setName( String pic)
+    {
+        name = pic;
+    }
+}
